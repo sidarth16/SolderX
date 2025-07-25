5223f39a16c3c827106f940dc58bdff4e0038fec
sidarth16 <ssidarth1999@gmail.com>
2025-07-25 12:01:48 +0530

    Release: v0.1.1 with LICENSE and README fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="solderx",
-    version="0.1.0",
+    version="0.1.1",
     description="⚡️ SolderX – Melt Imports. Solder Solidity. Flatten Everything 🔥",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
